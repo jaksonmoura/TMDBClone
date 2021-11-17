816013c8da2917b1efe715c9e85fba3ccd10dab7
Jakson Moura <jakson.moura@ati.to.gov.br>
2021-11-17 16:57:16 -0300

    search grid css

---
+++ src/components/MovieGrid/MovieGrid.styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+
+export const MoviesWrapper = styled.div`
+  width: 100%;
+  ${'' /* padding: 0 60px; */}
+  margin-bottom: 100px;
+`;
+
+export const ListTitle = styled.h2`
+  margin-bottom: 10px;
+  margin-top: 0;
+`;
+
+export const Movies = styled.div`
+  width: 100%;
+  display: grid;
+  gap: 20px;
+  grid-template-columns: repeat(auto-fit, minmax(180px, 1fr))
+`;
+
+
+export const ListHeader = styled.div`
+  display: inline-flex;
+  width: 100%;
+  align-items: center;
+  gap: 20px;
+
+  ul{
+    display: inline-flex;
+    gap: 20px;
+    align-items: center;
+    margin-bottom: 10px;
+    li{
+
+      a{
+        font-size: 1.8rem;
+      }
+    }
+  }
+`;
