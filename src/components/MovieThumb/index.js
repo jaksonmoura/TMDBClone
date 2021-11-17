a110050022241330b7948ac1f47d3a75f46eaf75
Jakson Moura <jakson.moura@ati.to.gov.br>
2021-11-17 16:57:51 -0300

    Format release_date; add noimg

--- src/components/MovieThumb/index.js
+++ src/components/MovieThumb/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config'
 import {Thumb, ThumbImg, Title, ReleaseDate} from './MovieThumb.styles'
 
-const MovieThumb = ({id, poster_path, title, release_date}) =>{
+const MovieThumb = ({id, poster_path, title, release_date, fullWidth = false}) =>{
     
     return(
         <Thumb href={`/movie/${id}`}>
-            <ThumbImg src={IMAGE_BASE_URL+POSTER_SIZE+poster_path}/>
+            <ThumbImg fullWidth={fullWidth} src={(poster_path ? IMAGE_BASE_URL+POSTER_SIZE+poster_path : "/noimg.jpg")}/>
             <Title>{title}</Title>
-            <ReleaseDate>{release_date}</ReleaseDate>
+            <ReleaseDate>{ release_date ? new Date(release_date).toLocaleDateString("en-US") : "Not informed" }</ReleaseDate>
         </Thumb>
     )
 }
