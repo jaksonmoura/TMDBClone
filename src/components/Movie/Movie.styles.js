477246d51c8704f5d93e682eae7e21ac87c3e692
Jakson Moura <jakson.moura@ati.to.gov.br>
2021-11-25 13:12:25 -0300

    Add finish layout of maininfo section

--- src/components/Movie/Movie.styles.js
+++ src/components/Movie/Movie.styles.js
@@ -1,27 +1,28 @@
 import styled from "styled-components";
 
 export const MovieBackdrop = styled.div`
-    width: 100%;
-    height: 500px;
-    border-radius: var(--defaultRadius);
-    overflow: hidden;
-    background-image: url(${props => props.backgroundImage ? props.backgroundImage : ""});
-    background-size: cover;
-    background-position: center center;
-    position: relative;
-    z-index: 1;
+  width: 100%;
+  height: 500px;
+  border-radius: var(--defaultRadius);
+  overflow: hidden;
+  background-image: url(${(props) =>
+    props.backgroundImage ? props.backgroundImage : ""});
+  background-size: cover;
+  background-position: center center;
+  position: relative;
+  z-index: 1;
 
-    &:before{
-        content: "";
-        width: 100%;
-        height: 60%;
-        background: linear-gradient(180deg, transparent, rgba(0,0,0, .5));
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        z-index: 1;
-    }
-`
+  &:before {
+    content: "";
+    width: 100%;
+    height: 60%;
+    background: linear-gradient(180deg, transparent, rgba(0, 0, 0, 0.5));
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    z-index: 1;
+  }
+`;
 
 export const MovieContainer = styled.div`
   width: clamp(300px, 100%, 1100px);
@@ -40,15 +41,134 @@ export const PosterSection = styled.div`
   display: inline-flex;
   flex-direction: column;
   margin-top: -100%;
+
+  .score {
+    width: 64px;
+    height: 64px;
+    display: inline-flex;
+    position: relative;
+
+    .progress-ring__circle {
+      transition: 0.5s stroke-dashoffset;
+      // axis compensation
+      transform: rotate(-90deg);
+      transform-origin: 50% 50%;
+      stroke: var(--mainColor);
+      stroke-dashoffset: 175;
+    }
+
+    .score-num {
+      width: 100%;
+      height: 50%;
+      align-self: center;
+      position: absolute;
+      text-align: center;
+      font-size: 2rem;
+      font-weight: 500;
+      top: calc(50% - 1.5rem);
+    }
+  }
+
+  .share-btn {
+    border: 1px solid var(--lightGrey);
+    border-radius: var(--defaultRadius);
+    display: inline-flex;
+    align-items: center;
+    gap: 8px;
+    justify-content: center;
+    color: var(--textColor);
+    height: 44px;
+    width: fit-content;
+    padding: 0 10px;
+    background: #fff;
+
+    &:hover {
+      background: var(--mainColor);
+      color: #fff;
+    }
+  }
 `;
 
 export const Poster = styled.img`
   width: 100%;
   border-radius: var(--defaultRadius);
   outline: 5px solid white;
+  margin-bottom: 1rem;
 `;
 
 export const OverviewSection = styled.div`
   width: 100%;
+`;
 
+export const OverviewMovieData = styled.div`
+  display: inline-flex;
+  gap: 30px;
+  align-items: center;
+
+  span {
+    position: relative;
+
+    &:after {
+      content: "";
+      width: 6px;
+      height: 6px;
+      background: #000;
+      border-radius: 6px;
+      position: absolute;
+      right: -16px;
+      top: calc(50% - 3px);
+      ${"" /* display: inline-block; */}
+
+      @media screen and (max-width: 600px) {
+        display: none;
+      }
+    }
+  }
+
+  ul {
+    display: inline-flex;
+    gap: 16px;
+    align-items: center;
+    li {
+      font-weight: 600;
+    }
+  }
+`;
+
+export const UserActions = styled.ul`
+  display: inline-flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+  gap: 1rem;
+
+  .other-actions{
+    display: inline-flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  ul {
+    display: inline-flex;
+    justify-content: space-between;
+    width: 100%;
+    gap: 1rem;
+
+    li {
+      width: calc(25% - 1rem);
+
+      &.double-size {
+        flex-grow: 1;
+      }
+
+      button {
+        width: 44px;
+        height: 44px;
+        border-radius: 50%;
+
+        &:hover {
+          outline: 2px solid var(--mainColor);
+        }
+      }
+    }
+  }
 `;
--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,37 +1,175 @@
-import React, {useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import API from '../../API'
-import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from '../../config'
-import { MovieBackdrop, MovieContainer, MainInfo, PosterSection, Poster, OverviewSection } from './Movie.styles';
+import API from "../../API";
+import { IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE } from "../../config";
+import {
+  MovieBackdrop,
+  MovieContainer,
+  MainInfo,
+  PosterSection,
+  Poster,
+  OverviewSection,
+  OverviewMovieData,
+  UserActions,
+} from "./Movie.styles";
+
+const movieObject = {
+  adult: false,
+  backdrop_path: "",
+  belongs_to_collection: {
+    id: 0,
+    name: "",
+    poster_path: "",
+    backdrop_path: "",
+  },
+  budget: 0,
+  genres: [
+    {
+      id: 0,
+      name: "",
+    },
+  ],
+  homepage: "",
+  id: 0,
+  imdb_id: "",
+  original_language: "en",
+  original_title: "",
+  overview: "n.",
+  popularity: 0.0,
+  poster_path: "",
+  production_companies: [
+    {
+      id: 0,
+      logo_path: "",
+      name: "",
+      origin_country: "",
+    },
+  ],
+  production_countries: [
+    {
+      iso_3166_1: "",
+      name: "",
+    },
+  ],
+  release_date: "",
+  revenue: 0,
+  runtime: 0,
+  spoken_languages: [
+    {
+      english_name: "",
+      iso_639_1: "",
+      name: "",
+    },
+  ],
+  status: "",
+  tagline: "",
+  title: "",
+  video: false,
+  vote_average: 0,
+  vote_count: 0,
+};
 
 const Movie = () => {
-    const { id } = useParams()
-    const [movie, setMovie] = useState({})
+  const { id } = useParams();
+  const [movie, setMovie] = useState(movieObject);
 
-    const fetchMovie = async (movieId) => {
-        const movieFetch = await API.fetchMovie(movieId)
-        setMovie(movieFetch)
-    }
+  function setProgress(percent) {
+    const circle = document.querySelector(".progress-ring__circle");
+    const radius = circle.r.baseVal.value;
+    const circumference = radius * 2 * Math.PI;
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.style.strokeDashoffset = circumference;
+    const offset = circumference - (percent / 100) * circumference;
+    circle.style.strokeDashoffset = offset;
+  }
 
-    useEffect(()=>{
-        fetchMovie(id)
-    }, [id])
+  const fetchMovie = async (movieId) => {
+    const movieFetch = await API.fetchMovie(movieId);
+    setMovie({ ...movieFetch });
+    console.log({ ...movieFetch });
+  };
 
-    return (
-        <>
-            <MovieBackdrop backgroundImage={IMAGE_BASE_URL+BACKDROP_SIZE+movie.backdrop_path} />
-            <MovieContainer>
-                <MainInfo>
-                    <PosterSection>
-                        <Poster src={IMAGE_BASE_URL+POSTER_SIZE+movie.poster_path} />
-                    </PosterSection>
-                    <OverviewSection>
-                        <h1>{movie.title}</h1>
-                        <p>{movie.overview}</p>
-                    </OverviewSection>
-                </MainInfo>
-            </MovieContainer>
-        </>
-    )
-}
-export default Movie
+  useEffect(() => {
+    fetchMovie(id);
+    setProgress(movie.vote_average * 10);
+  }, [id, movie.vote_average]);
+
+  return (
+    <>
+      <MovieBackdrop
+        backgroundImage={IMAGE_BASE_URL + BACKDROP_SIZE + movie.backdrop_path}
+      />
+      <MovieContainer>
+        <MainInfo>
+          <PosterSection>
+            <Poster
+              alt=""
+              src={IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path}
+            />
+            <UserActions>
+              <ul>
+                <li>
+                  <button aria-label="Favorite">
+                    <i className="material-icons">favorite_border</i>
+                  </button>
+                </li>
+                <li>
+                  <button aria-label="Favorite">
+                    <i className="material-icons">star_border</i>
+                  </button>
+                </li>
+                <li>
+                  <button aria-label="Favorite">
+                    <i className="material-icons">bookmark_border</i>
+                  </button>
+                </li>
+                <li>
+                  <button aria-label="Favorite">
+                    <i className="material-icons">list</i>
+                  </button>
+                </li>
+              </ul>
+              <div className="other-actions">
+                <div className="score">
+                  <svg class="progress-ring" width="64" height="64">
+                    <circle
+                      class="progress-ring__circle"
+                      stroke-width="6"
+                      fill="transparent"
+                      r="28"
+                      cx="32"
+                      cy="32"
+                    />
+                  </svg>
+                  <span className="score-num">
+                    {movie.vote_average > 0 ? movie.vote_average : "NR"}
+                  </span>
+                </div>
+                <a className="share-btn" href="#">
+                  <i className="material-icons">share</i> Share
+                </a>
+              </div>
+            </UserActions>
+          </PosterSection>
+          <OverviewSection>
+            <h1>{movie.title}</h1>
+
+            <OverviewMovieData>
+              <span>
+                {new Date(movie.release_date).toLocaleDateString("en-US")}
+              </span>
+              <span>{movie.runtime}m</span>
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li>{genre.name}</li>
+                ))}
+              </ul>
+            </OverviewMovieData>
+            <p>{movie.overview}</p>
+          </OverviewSection>
+        </MainInfo>
+      </MovieContainer>
+    </>
+  );
+};
+export default Movie;
